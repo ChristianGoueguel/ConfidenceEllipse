4e97189a01ffbd929f934a984aed76f9a12542ea
ChristianGoueguel <christian.goueguel@gmail.com>
2024-04-23 18:30:41 -0400

    Minor update

--- data-raw/DATASET.R
+++ data-raw/DATASET.R
@@ -1,6 +1,6 @@
 ## code to prepare `glass` dataset goes here
 glass <-
   readr::read_csv("~/Documents/Projects/Packages/data/glass.csv", show_col_types = FALSE) %>%
-  purrr::modify_at("glassType", as_factor)
+  purrr::modify_at("glassType", as.factor)
 
 usethis::use_data(glass, overwrite = TRUE)
